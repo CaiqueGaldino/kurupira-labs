6501952973c3a4308b6cff308985ea46136043f1
CaiqueGaldino <cont.caique@gmail.com>
2026-02-06 23:41:08 -0300

    fix: hidratação

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
-import { Bebas_Neue } from "next/font/google";
+import { Bebas_Neue, Barlow_Condensed } from "next/font/google";
 import "./globals.css";
 
 const bebasNeue = Bebas_Neue({
   variable: "--font-bebas",
   subsets: ["latin"],
   weight: "400",
+});
+
+const barlowCondensed = Barlow_Condensed({
+  variable: "--font-barlow",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -20,14 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-      </head>
       <body
-        className={`${bebasNeue.variable} antialiased`}
-        style={{ fontFamily: "'Barlow Condensed', sans-serif" }}
+        className={`${bebasNeue.variable} ${barlowCondensed.variable} antialiased`}
       >
         {children}
       </body>
